1e3b9cb679dc0a203a7b550bd628e4602656c7b7
Direil <97620806+Direil@users.noreply.github.com>
2022-06-15 02:55:20 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 def test_add_to_cart_button_is_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.implicitly_wait(5)
     browser.get(link)
+    time.sleep(30)
     basket = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert basket, "Кнопка добавления в корзину не была найдена."
